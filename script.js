cc5dcebe6eb285c6174717518c094b2bc4a20d9b
Vinicius Oliveira <157658093+viniciusN444@users.noreply.github.com>
2024-08-06 09:22:29 -0300

    Começando a fazer o script do site

---
+++ script.js
@@ -0,0 +1,4 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+let sizePassoword = document.querySelector("#valor");
+let password = document.querySelector("#password");
